2d13993130f96f5e8c81730fd875081b207dfc9f
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-08-28 12:06:08 +0700

    mengedit infixEval(), evalFormula(), dan update(), menambahkan applyFunction() dan fungsi rumus spreadsheet pada variable objek spreadsheetFunctions

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const infixToFunction = {
   '/': (x, y) => x / y,
 };
 
-const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator]);
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+const highPrecedence = (str) => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str2 === str ? str : highPrecedence(str2);
+};
 
 const isEven = (num) => num % 2 === 0;
 
@@ -20,8 +26,33 @@ const median = (nums) => {
   return isEven(length) ? (sorted[middle] + sorted[middle + 1]) / 2 : sorted[Math.ceil(middle)];
 };
 
-const spreadsheetFunctions = { sum, average, median };
+const spreadsheetFunctions = { 
+  "" : nums => nums,
+  sum, 
+  average, 
+  median, 
+  even: (nums) => nums.filter(isEven),
+  firsttwo: (nums) => nums.slice(0, 2),
+  lasttwo: (nums) => nums.slice(-2),
+  has2: nums => nums.includes(2),
+  increment: nums => nums.map(num => num + 1),
+  someeven: nums => nums.some(isEven),
+  everyeven: nums => nums.every(isEven),
+  random: nums => Math.floor(Math.random() * nums[1]) + nums[0],
+  range: nums => range(nums[0], nums[1]),
+  nodupes: nums => [...new Set(nums).values()],
+};
 
+const applyFunction = (str) => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = (args) => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
+};
+ 
 const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
@@ -38,6 +69,8 @@ const evalFormula = (x, cells) => {
   const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex, (match) => idToText(match.toUpperCase()));
+  const functionExpanded = applyFunction(cellExpanded);
+  return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 };
 
 window.onload = () => {
@@ -67,5 +100,6 @@ const update = (event) => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
   if (!value.includes(element.id) && value.startsWith("=")) {
+    element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
   }
 };
